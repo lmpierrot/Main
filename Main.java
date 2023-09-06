41a1bd4c6623d58eb43171a79c873040b56d8bb1
Marc Pierrot <106396509+lmpierrot@users.noreply.github.com>
2023-09-05 20:40:14 -0400

    Intro To Java
    Data type Print

---
+++ Main.java
@@ -0,0 +1,46 @@
+class Main {
+    public static void main(String[] args) {
+        // Place your code within this method
+        byte by = 127;
+        System.out.println(by);
+        byte fy = -128;
+        System.out.println(fy);
+
+        short s = 32_767;
+        System.out.println(s);
+        short f = -32_768;
+        System.out.println(f);
+
+        int i = 2_147_483_647;
+        System.out.println(i);
+        int r = -2_147_483_648;
+        System.out.println(r);
+
+        long l = 9_223_372_036_854_775_807L;
+        System.out.println(l);
+
+        double d = 3839.34839;
+        System.out.println(d);
+
+        float h = 3839.34839F;
+        System.out.println(h);
+        float g = 3.14F;
+        System.out.println(g);
+
+        boolean on = false;
+        System.out.println(on);
+        boolean of = true;
+        System.out.println(of);
+
+        char c = 'a';
+        System.out.println(c);
+        char t = 'z';
+        System.out.println(t);
+
+        String sentence = "the quick brown fox jumps over the lazy dog";
+        System.out.println(sentence);
+        String phrase = "I mastered Java data types!";
+        System.out.println(phrase);
+
+    } // End of Main method
+} // End of Main class
